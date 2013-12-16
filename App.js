3bda4325fbb187fa5e7c46defd07f13c3248f5ad
steve.rhoads@radcorp.com <steve.rhoads@radcorp.com>
2013-12-15 20:54:09 -0500

    Updated App.js

--- App.js
+++ App.js
@@ -4,5 +4,30 @@ Ext.define('CustomApp', {
     items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
     launch: function() {
         console.log("RallyGridInteractive-Launched");
+        this._loadStore();
+    },
+    _loadStore: function(){
+        console.log("Creating Store");
+        var myStore = Ext.create("Rally.data.wsapi.Store",{
+            model: 'Defect',
+            autoLoad: true,
+            listeners: {
+                scope: this,
+                load: function(store, data, success){
+                    console.log("Loading store");
+                    this._bindGrid(myStore);
+                }
+            },
+            fetch: ['FormattedID','Name','ScheduleState']
+        });
+    },
+	
+    _bindGrid: function(myStore){
+        console.log("Binding the grid");
+        var myGrid = Ext.create("Rally.ui.grid.Grid",{
+            store: myStore,
+            columnCfgs: ['FormattedID','Name']
+        });
+        this.add(myGrid);
     }
 });
